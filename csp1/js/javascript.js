a60ff4b9de753760d87b750f09d284d624da613e
lorenzojanrommel <lorenzojanrommel@gmail.com>
2017-12-12 08:27:29 +0800

    Capstone 1 | Day 1

---
+++ csp1/js/javascript.js
@@ -0,0 +1,23 @@
+/* button-collapse*/
+$( document ).ready(function(){
+	$(".button-collapse").sideNav();
+})
+
+/* video parallax */
+$(document).ready(function(){
+  	$('.parallax').parallax();
+});
+
+// character carousel
+$(document).ready(function(){
+  	$('.carousel').carousel();
+});
+// collapse
+ $(document).ready(function(){
+   	$('.collapsible').collapsible();
+ });
+// youtube
+// $(document).ready(function(){
+// // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+// $('.modal-trigger').leanModal();
+// });
