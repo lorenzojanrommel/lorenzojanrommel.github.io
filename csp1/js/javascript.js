7fd2c473012f27170558ebc2f6e8450ef7f83dbc
lorenzojanrommel <lorenzojanrommel@gmail.com>
2017-12-13 17:36:56 +0800

    Day 2 and 3

--- csp1/js/javascript.js
+++ csp1/js/javascript.js
@@ -21,3 +21,31 @@ $(document).ready(function(){
 // // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 // $('.modal-trigger').leanModal();
 // });
+
+
+// $(document).ready(function(){
+//     var videos = $("video");
+
+//     $('.modal-trigger').leanModal({
+//         complete: function() {
+//             videos.each(function(){
+//                 this.pause();
+//             });
+//         }
+//     });
+// });
+
+$(document).ready (function ($) {
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
+}); // end of jQuery name space
