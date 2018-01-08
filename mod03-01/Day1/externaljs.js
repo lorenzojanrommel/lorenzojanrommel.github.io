fe7a43a3697701efeb8969fcc17268339a5babc0
lorenzojanrommel <lorenzojanrommel@gmail.com>
2018-01-08 08:23:23 +0800

    jQuery Sample

---
+++ mod03-01/Day1/externaljs.js
@@ -0,0 +1,7 @@
+alert('Hello World from external js file');
+
+function changeHTML() {
+	document.getElementById('heading').innerHTML = 'Hello'
+	document.getElementById('content').innerHTML = 'World';
+}
+
