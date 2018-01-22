37c384f75e9e7f1341b7c0f0cfe9cc64050c73db
lorenzojanrommel <lorenzojanrommel@gmail.com>
2018-01-22 16:57:33 +0800

    CSP2v3

--- csp2/edit.php
+++ csp2/edit.php
@@ -1,14 +1,26 @@
 <?php
   $index = $_GET['index'];
-  $string = file_get_contents("assets/json/items.json");
-  $items = json_decode($string, true);
-  $items[$index]['name'] = htmlspecialchars($_POST['name']);
-  $items[$index]['description'] = htmlspecialchars($_POST['description']);
-  $items[$index]['price'] = htmlspecialchars($_POST['price']);
+  // $string = file_get_contents("assets/json/items.json");
+  // $items = json_decode($string, true);
+  require 'connectiondb.php';
+  $name = mysqli_real_escape_string($conn, $_POST['name']);
+  $description = mysqli_real_escape_string($conn, $_POST['description']);
+  $price = mysqli_real_escape_string($conn, $_POST['price']);
 
-  $file = fopen('assets/json/items.json', 'w');
-  fwrite($file, json_encode($items, JSON_PRETTY_PRINT));
-  fclose($file);
+  $sql = "UPDATE items SET
+                  name = '$name',
+                  description = '$description',
+                  price = '$price'
+                  WHERE id = '$index'";
+
+  // $items[$index]['name'] = htmlspecialchars($conn, $_POST['name']);
+  // $items[$index]['description'] = htmlspecialchars($conn, $_POST['description']);
+  // $items[$index]['price'] = htmlspecialchars($conn, $_POST['price']);
+
+  mysqli_query($conn, $sql) or die($mysqli_error($conn));
+  // $file = fopen('assets/json/items.json', 'w');
+  // fwrite($file, json_encode($items, JSON_PRETTY_PRINT));
+  // fclose($file);
   header('location: menu.php');
     // echo "<div class='col s4'>";
     // echo "<div class='card'>";
